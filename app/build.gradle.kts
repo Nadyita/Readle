7c920c646f67de642775c1a5de2ed769b5c5d1d5
Nadyita <nadyita@hodorraid.org>
2025-11-03 10:19:40 +0100

    Release v0.1.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.readle.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.1.10"
+        versionCode = 9
+        versionName = "0.1.11"
 
         testInstrumentationRunner = "com.readle.app.HiltTestRunner"
         vectorDrawables {
