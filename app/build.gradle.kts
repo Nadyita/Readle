91f780a8859381fd4aa23950abc23bca7c1c16cd
Nadyita <nadyita@hodorraid.org>
2025-11-07 08:15:33 +0100

    Release v0.1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.readle.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 15
-        versionName = "0.1.17"
+        versionCode = 16
+        versionName = "0.1.18"
 
         testInstrumentationRunner = "com.readle.app.HiltTestRunner"
         vectorDrawables {
