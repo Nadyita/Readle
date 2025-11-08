629ca1a3e2f76093a4b5df6b63697cc8f283eaa0
Nadyita <nadyita@hodorraid.org>
2025-11-08 09:52:24 +0100

    Release v0.1.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.readle.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 20
-        versionName = "0.1.22"
+        versionCode = 21
+        versionName = "0.1.23"
 
         testInstrumentationRunner = "com.readle.app.HiltTestRunner"
         vectorDrawables {
